0cbc843bde6512f1276addc513d10083f5fda539
Anastasia Keyzerova <key-nastia@mail.ru>
2023-07-16 20:35:30 +0300

    HomeTask 3 has been corrected

--- HomeTask3_6.py
+++ HomeTask3_6.py
@@ -5,7 +5,7 @@ mothers_height = np.array([178, 165, 165, 173, 168, 155, 160, 164, 178, 175])
 daughters_height = np.array([175, 167, 154, 174, 178, 148, 160, 167, 169, 170])
 n1 = len(mothers_height)
 n2 = len(daughters_height)
-D = (np.var(mothers_height) + np.var(daughters_height)) / 2
+D = (np.var(mothers_height, ddof=1) + np.var(daughters_height, ddof=1)) / 2
 SE = np.sqrt(D / n1 + D / n2)
 delta = np.mean(mothers_height) - np.mean(daughters_height)
 t = stats.t.ppf(0.975, n1 + n2 - 2)
